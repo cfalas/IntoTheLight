52c372f010f4005ea041ffd59e7f5cdb85ce0f37
Ekorrette <missmysss@gmail.com>
2023-02-12 19:00:09 +0000

    .

--- networking.h
+++ networking.h
@@ -50,7 +50,7 @@ class Websocket{
     }
     void ConnectServer(){
 	    EmscriptenWebSocketCreateAttributes ws_attrs = {
-			"ws://213.133.90.205:8765/",
+			"ws://game.tasosfalas.com:8765/",
 			NULL,
 			EM_TRUE
 		};
